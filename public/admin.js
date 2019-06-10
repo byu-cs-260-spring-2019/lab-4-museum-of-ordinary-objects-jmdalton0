c5685be4bfde2f086192fa00d9c33d6c67d6afbb
Jesse Dalton <jmdalton0@gmail.com>
2019-06-09 19:23:24 -0600

    battery dying

--- public/admin.js
+++ public/admin.js
@@ -1,5 +1,31 @@
 var app = new Vue({
   el: '#admin',
-  data: {},
-  methods: {}
+  data: {
+    title: "",
+    selected:  "",
+    addItem: null,
+    photos: [
+      {name: 'baseball', id: 1, path: 'baseball.jpg'},
+      {name: 'car', id: 2, path: 'car.jpg'},
+      {name: 'glasses', id: 3, path: 'glasses.jpg'},
+      {name: 'paintbrush', id: 4, path: 'paintbrush.jpg'},
+      {name: 'pen', id: 5, path: 'pen.jpg'},
+      {name: 'scissors', id: 6, path: 'scissors.jpg'},
+      {name: 'shovel', id: 7, path: 'shovel.jpg'},
+      {name: 'slinky', id: 8, path: 'slinky.jpg'},
+    ],
+  },
+  methods: {
+    async addNewItem(){
+      try {
+        let result = await axios.post('/api/items', {
+          title: this.title,
+          path: this.selected.path
+        });
+        this.addItem = result.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  }
 });
--- server.js
+++ server.js
@@ -1,20 +1,32 @@
+const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
 const express = require('express');
-const bodyParser = require("body-parser");
+
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+);
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
 
-app.use(express.static('public'));
 
-const mongoose = require('mongoose');
+// Create a new item in the museum: takes a title and a path to an image.
+var db = firebase.firestore();
+var itemsRef = db.collection('items');
 
-// connect to the database
-mongoose.connect('mongodb://localhost:27017/museum', {
-  useNewUrlParser: true
+app.post('/api/items', async (req, res) => {
+    try {
+        let querySnapshot = await itemsRef.get();
+        let numRecords = querySnapshot.docs.length;
+        let item = {
+            id: numRecords + 1,
+            title: req.body.title,
+            path: req.body.path
+        };
+        itemsRef.doc(item.id.toString()).set(item);
+        res.send(item);
+      } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+      }
 });
-
-
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+exports.app = functions.https.onRequest(app);
